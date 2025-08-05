53d3a07c66e7d72271f09063e1bc26baa2975dd1
Aditya Mishra <136081658+AdityaMISHRA2803@users.noreply.github.com>
2025-08-06 01:43:37 +0530

    Update socket.js

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,34 +7,28 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://yap-yap-chat.vercel.app"
-    ],
-    credentials: true
-  }
+    origin: ["http://localhost:5173"],
+  },
 });
-
-// used to store online users
-const userSocketMap = {}; // { userId: socketId }
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
 io.on("connection", (socket) => {
-  console.log("✅ A user connected:", socket.id);
+  console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) {
-    userSocketMap[userId] = socket.id;
-  }
+  if (userId) userSocketMap[userId] = socket.id;
 
-  // Notify all clients about currently online users
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("❌ A user disconnected:", socket.id);
+    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
