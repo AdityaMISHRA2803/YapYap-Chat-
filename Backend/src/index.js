2090988758523f8e8968d7bc66d7a7e752b2eeac
Aditya Mishra <136081658+AdityaMISHRA2803@users.noreply.github.com>
2025-08-06 22:59:16 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -27,8 +27,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173", // frontend dev server
+   cors({
+    origin: [
+      "http://localhost:5173",
+      "https://yapyap-chat-wjuo.onrender.com"
+    ],
     credentials: true,
   })
 );
