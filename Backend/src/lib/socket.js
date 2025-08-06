efc92b2f38ac90ec94d5e23951308da28ff58413
Aditya Mishra <136081658+AdityaMISHRA2803@users.noreply.github.com>
2025-08-06 22:59:50 +0530

    Update socket.js

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -6,8 +6,13 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
+   cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://yapyap-chat-wjuo.onrender.com"  // <-- add this
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
