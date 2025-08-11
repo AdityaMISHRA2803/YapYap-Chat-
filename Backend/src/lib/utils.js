0fc3109c9f8de404bbb9d3c1210ee0af53f111be
Aditya Mishra <136081658+AdityaMISHRA2803@users.noreply.github.com>
2025-08-11 20:20:16 +0530

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,20 +1,3 @@
-// import jwt from 'jsonwebtoken';
-
-// export const generateToken = (userId, res) => {
-//     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//         expiresIn: '7d',
-//     });
-
-//     res.cookie("jwt",token,{
-//         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-//         httpOnly: true, // prevent xss attacks cross-site scripting attacks
-//         sameSite: "strict", // prevent cross-site request forgery attacks
-//         secure: process.env.NODE_ENV !== "development" // only send cookie over https in production
-//     });
-//     return token;
-
-// };
-
 
 import jwt from "jsonwebtoken";
 
@@ -27,13 +10,6 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d", // Token valid for 7 days
   });
-
-  // res.cookie("jwt", token, {
-  //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-  //   httpOnly: true, // Prevents access from JavaScript (mitigates XSS)
-  //   sameSite: "Strict", // CSRF protection
-  //   secure: process.env.NODE_ENV === "production", // Only use HTTPS in production
-  // });
 
 
 res.cookie("jwt", token, {
