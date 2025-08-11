178879806f4adb3d9697d7cfd36a8b07160061e3
Aditya Mishra <136081658+AdityaMISHRA2803@users.noreply.github.com>
2025-08-11 20:15:01 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -49,7 +49,7 @@ app.use("/api/messages", messageRoutes);
 // ✅ Serve frontend static files in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
